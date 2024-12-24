c79dc1e5fe62a96f6e90f56bf3e5c26d823b4084
DomenechSoler <domenechsolerkilian@fpllefia.com>
2024-12-24 19:40:31 +0100

    Definir el array partidas con datos de prueba.

--- src/componentes/tablaPartidas.jsx
+++ src/componentes/tablaPartidas.jsx
@@ -1,4 +1,26 @@
 export default function tablaPartidas(){
+    const partidas = [
+        {
+            avatar: "https://www.svgrepo.com/show/384669/account-avatar-profile-user-13.svg",
+            nick: "Juanito",
+            puntos: 250,
+            fecha: "15/01/2024"
+        },
+        {
+            avatar: "https://www.svgrepo.com/show/384672/account-avatar-profile-user-7.svg",
+            nick: "Lucia89",
+            puntos: 500,
+            fecha: "10/11/2024"
+        },
+        {
+            avatar: "https://www.svgrepo.com/show/384676/account-avatar-profile-user-6.svg",
+            nick: "GamerPro",
+            puntos: 750,
+            fecha: "05/03/2024"
+        }
+            
+    ]
+
     return "hola"
 
 }
