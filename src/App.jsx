f85620e2247f29e19b89eb7ad9cedd58ec3383f0
DomenechSoler <domenechsolerkilian@fpllefia.com>
2024-12-27 18:47:05 +0100

    Configura rutas con React Router y Añade menú de navegación

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import './App.css'
-import TablaPartidas from './componentes/tablaPartidas'
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import VistaInicio from './vistas/vistaInicio';
+import VistaJuego from './vistas/vistaJuego';
+import VistaPartidas from './vistas/vistaPartidas';
+import VistaRanking from './vistas/vistaRanking';
 
 function App() {
-
-
   return (
-    <>
-      <TablaPartidas/>  
-    </>
-  )
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item"><Link className="nav-link" to="/">Inicio</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/juego">Juego</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/partidas">Partidas</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/ranking">Ranking</Link></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<VistaInicio />} />
+          <Route path="/juego" element={<VistaJuego />} />
+          <Route path="/partidas" element={<VistaPartidas />} />
+          <Route path="/ranking" element={<VistaRanking />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
